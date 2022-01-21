1555a52ac4c47f006243d7719e98adcb7c5e37fc
ozcanozgur <ozcan.ozgur1@huawei.com>
2022-01-21 13:16:04 +0300

    test

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Love u Ugur!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
