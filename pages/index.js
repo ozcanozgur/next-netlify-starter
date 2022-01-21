4de2d1a2a71d3b348420314e88ddac7fd4a17c88
ozcanozgur <ozcan.ozgur1@huawei.com>
2022-01-21 16:49:24 +0300

    google

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -8,16 +9,31 @@ export default function Home() {
       <Head>
         <title>Love u Ipek!</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta name="google-site-verification" content="9ztrFaH9SagriZscLtd90OFgNDGyS-Wr8p-RzjTyLOs" />
       </Head>
 
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Aşkımsın
-        </p>
-      </main>
+      <main >
+        <>
+          <nav className='flex items-center flex-wrap bg-green-300 p-3 '>
+            <Link href='/'>
+              <a className='inline-flex items-center p-2 mr-4 '>
+                <svg
+                  viewBox='0 0 24 24'
+                  xmlns='http://www.w3.org/2000/svg'
+                  className='fill-current text-white h-8 w-8 mr-2'
+                >
+                  <path d='M12.001 4.8c-3.2 0-5.2 1.6-6 4.8 1.2-1.6 2.6-2.2 4.2-1.8.913.228 1.565.89 2.288 1.624C13.666 10.618 15.027 12 18.001 12c3.2 0 5.2-1.6 6-4.8-1.2 1.6-2.6 2.2-4.2 1.8-.913-.228-1.565-.89-2.288-1.624C16.337 6.182 14.976 4.8 12.001 4.8zm-6 7.2c-3.2 0-5.2 1.6-6 4.8 1.2-1.6 2.6-2.2 4.2-1.8.913.228 1.565.89 2.288 1.624 1.177 1.194 2.538 2.576 5.512 2.576 3.2 0 5.2-1.6 6-4.8-1.2 1.6-2.6 2.2-4.2 1.8-.913-.228-1.565-.89-2.288-1.624C10.337 13.382 8.976 12 6.001 12z' />
+                </svg>
+                <span className='text-xl text-white font-bold uppercase tracking-wide'>
+                  TalwindCSS
+                </span>
+              </a>
+            </Link>
+          </nav>
+        </>
+      </main >
 
       <Footer />
-    </div>
+    </div >
   )
 }
